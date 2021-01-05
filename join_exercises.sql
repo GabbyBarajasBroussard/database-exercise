7a41c8007ae94670e04ec22f30b60135c3da4f7d
Gabby Broussard <gabriella.b.broussard@gmail.com>
2021-01-05 09:49:28 -0600

    join exercises

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,16 +1,33 @@
-/*Create a file named join_exercises.sql to do your work in.
+## Create a file named join_exercises.sql to do your work in.
+use join_example_db;
 
-Join Example Database
-Use the join_example_db. Select all the records from both the users and roles tables.
+## Join Example Database
+		## Use the join_example_db. Select all the records from both the users and roles tables.
+				SELECT * FROM users AS u 
+					JOIN roles AS r ON r.id =u.role_id;
 
-Use join, left join, and right join to combine results from the users and roles tables as we did in the lesson. Before you run each query, guess the expected number of results.
+		##  Use join, left join, and right join to combine results from the users and roles tables as we did in the lesson. Before you run each query, guess the expected number of results.
 
-Although not explicitly covered in the lesson, aggregate functions like count can be used with join queries. Use count and the appropriate join type to get a list of roles along with the number of users that has the role. Hint: You will also need to use group by in the query.
 
-Employees Database
-Use the employees database.
+					## Join, I expect 4 rows of names, emails, and role of the company.
+						SELECT * FROM users AS u
+							JOIN roles AS r ON r.id = u.role_id ;
+					## Left Join-- I expect 6 rows this time because the last query did not include those without roles. 
+						SELECT * FROM users AS u
+							LEFT JOIN roles AS r ON r.id = u.role_id;
+					## Right Join-- I expect 5 rows because this one lists all the roles even if no one is in the role.
+						SELECT * FROM users AS u
+							RIGHT JOIN roles AS r ON r.id = u.role_id;
+		##		Although not explicitly covered in the lesson, aggregate functions like count can be used with join queries. Use count and the appropriate join type to get a list of roles along 	with the number of users that has the role. Hint: You will also need to use group by in the query.
+		SELECT r.name, COUNT(roles.name) AS 'Number of People in Each Role' FROM users AS u
+			RIGHT JOIN roles AS r ON r.id = u.role_id
+			GROUP BY r.name;
 
-Using the example in the Associative Table Joins section as a guide, write a query that shows each department along with the name of the current manager for that department.
+
+## Employees Database
+## Use the employees database.
+USE employees;
+/* Using the example in the Associative Table Joins section as a guide, write a query that shows each department along with the name of the current manager for that department.
 
 
   Department Name    | Department Manager
@@ -24,18 +41,40 @@ Using the example in the Associative Table Joins section as a guide, write a que
   Quality Management | Dung Pesch
   Research           | Hilary Kambil
   Sales              | Hauke Zhang
-Find the name of all departments currently managed by women.
+  
+*/
+SELECT dept_name AS 'DEPARTMENT NAME', 
+CONCAT(first_name, ' ', last_name ) AS 'DEPARTMENT MANAGER'
+FROM departments
+JOIN dept_manager 
+	ON departments.dept_no = dept_manager.dept_no
+JOIN employees 
+	ON employees.emp_no = dept_manager.emp_no 
+		AND dept_manager.to_date > curdate()
+ORDER BY dept_name; 
 
+/* Find the name of all departments currently managed by women. 
 
 Department Name | Manager Name
 ----------------+-----------------
 Development     | Leon DasSarma
 Finance         | Isamu Legleitner
 Human Resources | Karsetn Sigstam
-Research        | Hilary Kambil
-Find the current titles of employees currently working in the Customer Service department.
+Research        | Hilary Kambil */
+USE employees;
+SELECT dept_name, CONCAT(first_name, ' ', last_name ) AS dept_manager
+FROM departments
+JOIN dept_manager 
+	ON departments.dept_no = dept_manager.dept_no
+JOIN employees 
+	ON employees.emp_no = dept_manager.emp_no 
+		AND dept_manager.to_date > curdate() 
+		AND employees.gender = 'F'
+ORDER BY dept_name; 
 
 
+
+/* Find the current titles of employees currently working in the Customer Service department.
 Title              | Count
 -------------------+------
 Assistant Engineer |    68
@@ -45,8 +84,24 @@ Senior Engineer    |  1790
 Senior Staff       | 11268
 Staff              |  3574
 Technique Leader   |   241
-Find the current salary of all current managers.
+*/
 
+SELECT title AS 'Title', 
+COUNT(*)  AS 'Count'
+FROM titles
+JOIN employees 
+	ON titles.emp_no = employees.emp_no 
+JOIN dept_emp
+	ON employees.emp_no = dept_emp.emp_no	
+JOIN departments 
+	ON dept_emp.dept_no = departments.dept_no
+		WHERE departments.dept_no LIKE '%009'
+		AND titles.to_date > curdate()
+		AND dept_emp.to_date > curdate()
+GROUP BY title
+;
+
+/* Find the current salary of all current managers.
 
 Department Name    | Name              | Salary
 -------------------+-------------------+-------
@@ -58,10 +113,21 @@ Marketing          | Vishwani Minakawa | 106491
 Production         | Oscar Ghazalie    |  56654
 Quality Management | Dung Pesch        |  72876
 Research           | Hilary Kambil     |  79393
-Sales              | Hauke Zhang       | 101987
-Find the number of current employees in each department.
+Sales              | Hauke Zhang       | 101987 */
 
+SELECT dept_name AS 'Department Name', CONCAT(first_name, ' ', last_name ) AS Name , salaries.salary AS Salary
+FROM departments
+JOIN dept_manager 
+	ON departments.dept_no = dept_manager.dept_no
+JOIN employees 
+	ON employees.emp_no = dept_manager.emp_no 
+		AND dept_manager.to_date > curdate()
+JOIN salaries 
+	ON employees.emp_no = salaries.emp_no 
+		AND salaries.to_date > curdate()
+ORDER BY dept_name; 
 
+/* Find the number of current employees in each department.
 +---------+--------------------+---------------+
 | dept_no | dept_name          | num_employees |
 +---------+--------------------+---------------+
@@ -74,39 +140,99 @@ Find the number of current employees in each department.
 | d007    | Sales              | 37701         |
 | d008    | Research           | 15441         |
 | d009    | Customer Service   | 17569         |
-+---------+--------------------+---------------+
-Which department has the highest average salary? Hint: Use current not historic information.
++---------+--------------------+---------------+ */
+SELECT departments.dept_no, departments.dept_name, COUNT(*) AS num_employees
+FROM departments
+JOIN dept_emp 
+	ON dept_emp.dept_no = departments.dept_no
+JOIN employees 
+	ON dept_emp.emp_no = employees.emp_no 
+		AND dept_emp.to_date > CURDATE()
+GROUP by departments.dept_no;
+
+/* Which department has the highest average salary? Hint: Use current not historic information.
 
 
 +-----------+----------------+
 | dept_name | average_salary |
 +-----------+----------------+
 | Sales     | 88852.9695     |
-+-----------+----------------+
-Who is the highest paid employee in the Marketing department?
++-----------+----------------+ */
+SELECT dept_name, AVG (salary) AS 'average_salary'
+FROM departments
+JOIN dept_emp 
+	ON departments.dept_no= dept_emp.dept_no 
+		AND dept_emp.to_date > curdate()
+JOIN salaries 
+	ON dept_emp.emp_no = salaries.emp_no
+	AND salaries.to_date > curdate() 
+GROUP BY dept_name
+ORDER BY average_salary DESC
+LIMIT 1;
 
-
+/* Who is the highest paid employee in the Marketing department?
 +------------+-----------+
 | first_name | last_name |
 +------------+-----------+
 | Akemi      | Warwick   |
-+------------+-----------+
-Which current department manager has the highest salary?
++------------+-----------+ */
+SELECT first_name, last_name
+FROM employees
+JOIN dept_emp 
+	ON dept_emp.emp_no = employees.emp_no
+		AND dept_emp.to_date > curdate()
+JOIN departments 
+	ON departments.dept_no= dept_emp.dept_no
+		AND departments.dept_no LIKE '%01'
+JOIN salaries 
+	ON employees.emp_no = salaries.emp_no
+		AND salaries.to_date > curdate()
+ORDER BY salary DESC
+LIMIT 1;
 
-
+/* Which current department manager has the highest salary?
 +------------+-----------+--------+-----------+
 | first_name | last_name | salary | dept_name |
 +------------+-----------+--------+-----------+
 | Vishwani   | Minakawa  | 106491 | Marketing |
-+------------+-----------+--------+-----------+
-Bonus Find the names of all current employees, their department name, and their current manager's name.
++------------+-----------+--------+-----------+ */
+SELECT first_name, last_name, salary, dept_name
+FROM employees
+JOIN dept_manager 
+	ON dept_manager.emp_no = employees.emp_no
+JOIN salaries
+	ON salaries.emp_no = dept_manager.emp_no
+		AND salaries.to_date > curdate()
+JOIN departments
+	ON departments.dept_no = dept_manager.dept_no 
+		AND dept_manager.to_date > curdate()
+ORDER BY salary DESC
+LIMIT 1;
+
+
+/* Bonus Find the names of all current employees, their department name, and their current manager's name.
 
 
 240,124 Rows
 
 Employee Name | Department Name  |  Manager Name
 --------------|------------------|-----------------
- Huan Lortz   | Customer Service | Yuchang Weedman
+ Huan Lortz   | Customer Service | Yuchang Weedman */
+SELECT 
+CONCAT(e.first_name, ' ', e.last_name) AS 'Employee Name',
+d.dept_name AS 'Department Name',
+CONCAT (ee.first_name,' ', ee.last_name) AS 'Manager Name'
+FROM dept_emp AS de
+JOIN employees AS e
+	ON de.emp_no = e.emp_no
+JOIN departments AS d
+	ON de.dept_no = d.dept_no
+JOIN dept_manager AS dm
+	ON de.dept_no = dm.dept_no 
+		AND dm.to_date > curdate()
+JOIN employees AS ee
+	ON dm.emp_no = ee.emp_no
+WHERE de.to_date > curdate();
 
- .....
-Bonus Who is the highest paid employee within each department. */
+
+/* Bonus Who is the highest paid employee within each department. */
